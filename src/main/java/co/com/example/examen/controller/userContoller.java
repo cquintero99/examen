24a205dbbe689d20a2ffe7ce3ad570b8dbd22d0f
cquintero99 <ciroalfonsoqude@ufps.edu.co>
2022-12-23 11:43:58 -0500

    agrego 4 DELETE

--- src/main/java/co/com/example/examen/controller/userContoller.java
+++ src/main/java/co/com/example/examen/controller/userContoller.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -47,6 +48,14 @@ public class UserContoller {
 			
 		}
 		return null;
+	}
+	
+	@DeleteMapping("/{user}/bills/{bill_id}")
+	public void deleteById(@PathVariable String user,@PathVariable Integer bill_id){
+		Optional<User>userCurrent=userRepository.findByUsername(user);
+		if(userCurrent.isPresent()) {
+			 billRepository.deleteById(bill_id);
+		}
 	}
 	
 	
